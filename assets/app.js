0bd99178ca0b242aa05fef980b061627c44e46db
Orel <aurelien.chirot@idee-lab.fr>
2018-08-17 11:23:25 +0200

    BUG : adding icon to datetimepicker

--- assets/app.js
+++ assets/app.js
@@ -106,6 +106,21 @@ $(document).ready(function()
 	/*
 		A datetime picker for the "when" field
 	*/
+
+	$.extend(true, $.fn.datetimepicker.defaults, {
+		icons: {
+			time: 'far fa-clock',
+			date: 'far fa-calendar',
+			up: 'fas fa-arrow-up',
+			down: 'fas fa-arrow-down',
+			previous: 'fas fa-chevron-left',
+			next: 'fas fa-chevron-right',
+			today: 'fas fa-calendar-check',
+			clear: 'far fa-trash-alt',
+			close: 'far fa-times-circle'
+		}
+	});
+
 	$('#when').datetimepicker({
 		inline: true,
 		sideBySide: true,
